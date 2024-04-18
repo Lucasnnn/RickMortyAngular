907571978995e117cc397df416605341154dc491
lucas de oliveira santos <los.inovation@gmail.com>
2024-04-18 15:42:13 -0300

    fix

--- src/app/shared/components/character-list/character-list.component.ts
+++ src/app/shared/components/character-list/character-list.component.ts
@@ -3,7 +3,7 @@ import { Character } from '../../models/character.type';
 
 @Component({
   selector: 'app-character-list',
-  styleUrls: ['./character-list.component.scss'],
+  templateUrl: './character-list.component.html',
 })
 export class CharacterListComponent {
   @Input() characters: Character[];
