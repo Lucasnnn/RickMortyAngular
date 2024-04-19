8933f0aec4d42acd2d993d21779333ab676e51db
lucas de oliveira santos <los.inovation@gmail.com>
2024-04-18 23:39:32 -0300

    feat: count favs using async pipe

--- src/app/core/components/menu/menu.component.ts
+++ src/app/core/components/menu/menu.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { FavoritesService } from '../../services/favorites.service';
+import { map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss'],
 })
-export class MenuComponent {}
+export class MenuComponent {
+  idsLength$: Observable<number>;
+
+  constructor(private _favSerivce: FavoritesService) {
+    this.idsLength$ = this._favSerivce.ids$.pipe(map((array) => array.length));
+  }
+}
